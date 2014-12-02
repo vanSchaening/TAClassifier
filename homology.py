32fcc3e796af782a985ac33478d457de4e15d3ae
Cass <cschaening@mit.edu>
2014-12-02 17:08:39 -0500

    Fixed some bugs

--- homology.py
+++ homology.py
@@ -1,30 +1,27 @@
-
 # ------------------------------------------------------------------------------
 # MAIN FUNCTION
 # ------------------------------------------------------------------------------
-
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--antitoxin', help="antitoxin faa") # antitoxin aa fasta
-    parser.add_argument('-t', '--toxin', help="toxin faa")         # toxin aa fasta
-    parser.add_argument('-o', '--output', help="output prefix")    # output prefix
-    parser.add_argument('-d', '--database', default=False,         # target database to
-                        help="phage genome database")              #    blast against
-    parser.add_argument('-e', '--evalue', type=int, default=20,    # evalue, default is 20
-                        help="expect value for alignments")  
+    parser.add_argument('-t', '--toxin', help="toxin faa") # toxin aa fasta
+    parser.add_argument('-o', '--output', help="output prefix") # output prefix
+    parser.add_argument('-d', '--database', default=False, # target database to
+                        help="phage genome database") # blast against
+    parser.add_argument('-e', '--evalue', type=int, default=20, # evalue, default is 20
+                        help="expect value for alignments")
     args = parser.parse_args()
 
-    homology(args.toxin, args.antitoxin, args.database, args.evalue, args.output)
-
-
+    t_out,a_out = homology(args.toxin, args.antitoxin, args.database, 
+                           args.evalue, args.output)
 # ------------------------------------------------------------------------------
 
 from Bio.Blast.Applications import NcbitblastnCommandline as BlastCommandLine
 def homology(toxin_faa,antitoxin_faa, # Input fasta aa files
-             database,                # Path to target database
-             evalue,                  # E-value parameter for BLAST
-             out):                    # Output prefix
+             database, # Path to target database
+             evalue, # E-value parameter for BLAST
+             out): # Output prefix
 
     # Run TBLASTN for toxin
     t_result = ".".join([out,"tblastn","toxin","xml"])
@@ -34,20 +31,21 @@ def homology(toxin_faa,antitoxin_faa, # Input fasta aa files
     runTBLASTN(toxin_faa,a_result,evalue,database)
 
     # Summarize results
-    t_scores = summarizeResults(t_results)
-    a_scores = summarizeResults(a_results)
+    t_scores = summarizeResults(t_result)
+    a_scores = summarizeResults(a_result)
+
     # Store results into output files
     t_out = ".".join([out,"homology","toxin","txt"])
     a_out = ".".join([out,"homology","antitoxin","txt"])
     storeResults(t_scores,t_out)
     storeResults(a_scores,a_out)
+
     # Return output filenames
     return t_out,a_out
 
 
 # ------------------------------------------------------------------------------
-
-# Run tblastn (align aa seqs to translated nucleotide database) for 
+# Run tblastn (align aa seqs to translated nucleotide database) for
 # given parameters
 def runTBLASTN(query_file,outfile,evalue,db):
     # Build blast command
@@ -56,29 +54,29 @@ def runTBLASTN(query_file,outfile,evalue,db):
                                    out=outfile,outfmt=5)
     # Run it, output will be stored in the files specified above
     stdout,stderr = blast_cline()
+    return
 
 # Store a single, best value for each query sequence
 from Bio.Blast import NCBIXML
 def summarizeResults(blast_results):
-    # Parse XML-formatted BLAST output
-    handle = open(blast_results)
-    records = NCBIXML.parse(handle)
-    # For each query, store the best alignment score
-    scores = dict()
-    for record in records:
-        name = record.query
-        for alignment in record.alignments:
-            scores[name] = max([ hsp.score for hsp in alignment.hsps ])
-    handle.close()
-    return scores    
+        # Parse XML-formatted BLAST output
+        handle = open(blast_results)
+        records = NCBIXML.parse(handle)
+        # For each query, store the best alignment score
+        scores = dict()
+        for record in records:
+            name = record.query
+            for alignment in record.alignments:
+                scores[name] = max([ hsp.score for hsp in alignment.hsps ])
+        handle.close()
+        return scores
 
 def storeResults(scores,outfile):
-    o = open(outfile,'w')
-    for name,score in scores.iteritems():
-        o.write(name+"\t"+str(score)+"\n")
-    o.close()
+    with open(outfile,'w') as o:
+        for name,score in scores.iteritems():
+            o.write(name+"\t"+str(score)+"\n")
+
 
 # ------------------------------------------------------------------------------
-
 if __name__ == '__main__':
     main()
