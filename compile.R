294fb28d3f5921d8b3ff93ccb1daaf10f3abe63f
Grace Yeo <yhtgrace@gmail.com>
2014-12-03 01:07:22 -0500

    Option to add inverted data points

--- compile.R
+++ compile.R
@@ -1,7 +1,7 @@
 library(plyr)
 
 features<-c("homology", "properties", "structure", "upalindromes", "essentiality")
-class<-c("positive", "negative")
+classes<-c("positive", "negative")
 feature_names<-c("locus_id", "refseq", "class", 
                  "homology.toxin", "homology.antitoxin", 
                  "properties.gene1.pi", "properties.gene2.pi", "properties.gene1.weight", "properties.gene2.weight", 
@@ -12,12 +12,43 @@ feature_names<-c("locus_id", "refseq", "class",
 import_by_refseq_and_class <- function(refseq, class) {
   filenames<-lapply(features, function(feature) paste(refseq, class, "features", feature, "txt", sep="."))
   tables<-llply(filenames, read.table)
+ 
+  options(warn=-1)
   data<-Reduce(function(x, y) merge(x, y, by=1), tables)
+  options(warn=1)
+  
   colnames(data)<-feature_names[c(1,4:length(feature_names))]
   data$class<-if(class=="positive") 1 else -1
   data$refseq<-refseq
-  data<-data[feature_names]  
+  data<-data[feature_names] # enforce order
   return(data)
+}
+
+add_inverted_negatives <- function(data) {
+  inverted<-data[data$class==-1,]
+  colnames(inverted)<-c("locus_id", "refseq", "class", 
+                        "homology.antitoxin", "homology.toxin", 
+                        "properties.gene2.pi", "properties.gene1.pi", "properties.gene2.weight", "properties.gene1.weight", 
+                        "structure.gene2.length", "structure.gene1.length", "structure.distance", "structure.overlap",
+                        "upalindromes.length", "upalindromes.distance", 
+                        "essentiality.antitoxin", "essentiality.toxin")
+  inverted<-inverted[feature_names]
+  inverted$locus_id<-paste(inverted$locus_id, "i", sep="_")
+  return(rbind(data, inverted))
+}
+
+add_inverted_positives <- function(data) {
+  inverted<-data[data$class==1,]
+  colnames(inverted)<-c("locus_id", "refseq", "class", 
+                        "homology.antitoxin", "homology.toxin", 
+                        "properties.gene2.pi", "properties.gene1.pi", "properties.gene2.weight", "properties.gene1.weight", 
+                        "structure.gene2.length", "structure.gene1.length", "structure.distance", "structure.overlap",
+                        "upalindromes.length", "upalindromes.distance", 
+                        "essentiality.antitoxin", "essentiality.toxin")
+  inverted<-inverted[feature_names]
+  inverted$class<-1
+  inverted$locus_id<-paste(inverted$locus_id, "i", sep="_") 
+  return(rbind(data, inverted))  
 }
 
 import_by_refseq <- function(refseq) {
@@ -25,7 +56,10 @@ import_by_refseq <- function(refseq) {
   return(Reduce(rbind, data))
 }
 
+args <- commandArgs(trailingOnly = TRUE) # use first argument as output file
 refseq<-"NC_011060"
 data<-import_by_refseq(refseq)
-
+#data<-add_inverted_negatives(data)
+#data<-add_inverted_positives(data)
+write.table(data, file=args[[1]], quote=FALSE, sep="\t", row.names=FALSE)
 
