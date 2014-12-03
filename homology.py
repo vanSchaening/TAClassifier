2ad55be4eeb62cfdb1150bed6ed49acaf9724128
Cass <cschaening@mit.edu>
2014-12-02 19:58:19 -0500

    Output from homology.py is now in toxin-antitoxin pairs, in file <prefix>.homology.txt. It still creates the files with separate toxin and antitoxin scores, just in case.

--- homology.py
+++ homology.py
@@ -34,6 +34,8 @@ def homology(toxin_faa,antitoxin_faa, # Input fasta aa files
     t_scores = summarizeResults(t_result)
     a_scores = summarizeResults(a_result)
 
+    joinScores(t_scores,a_scores,out)
+
     # Store results into output files
     t_out = ".".join([out,"homology","toxin","txt"])
     a_out = ".".join([out,"homology","antitoxin","txt"])
@@ -55,6 +57,39 @@ def runTBLASTN(query_file,outfile,evalue,db):
     # Run it, output will be stored in the files specified above
     stdout,stderr = blast_cline()
     return
+
+def joinScores(a_scores,t_scores,prefix):
+    # Get starting positions for each gene
+    import cPickle as pickle
+    with open(prefix+".startPositions.pkl",'rb') as s:
+        start = pickle.load(s)
+
+    # Write homology scores for all pairs, order genes in 
+    # a pair by position, not by toxin/antitoxin
+    mapfile = prefix+".mapping.txt"
+    outfile = prefix+".homology.txt"
+    with open(mapfile) as id_map, open(outfile,'w') as o:
+        for line in id_map:
+            if line.startswith("#"):
+                continue
+            locus,gene1,gene2 = line.strip().split()
+            if start[gene1] < start[gene2]:
+                o.write("\t".join([ locus,
+                                    score(gene1,a_scores,t_scores),
+                                    score(gene2,a_scores,t_scores) ])+"\n")
+            else:
+                o.write("\t".join([ locus,
+                                    score(gene2,a_scores,t_scores),
+                                    score(gene1,a_scores,t_scores) ])+"\n")
+    return outfile
+
+def score(gene,a_scores,t_scores):
+    if gene in a_scores:
+        return a_scores[gene]
+    elif gene in t_scores:
+        return t_scores[gene]
+    else:
+        return "0"
 
 # Store a single, best value for each query sequence
 from Bio.Blast import NCBIXML
