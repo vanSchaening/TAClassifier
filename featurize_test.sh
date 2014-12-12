dc9a4f13459485f675c7a30eceb5e8244957e267
vanSchaening <c.schaening@gmail.com>
2014-12-11 21:30:01 -0500

    featurizing script for test cases, to be included in predict.sh

---
+++ featurize_test.sh
@@ -0,0 +1,192 @@
+# Download files and compute features given a reference sequence 
+
+usage()
+{
+cat << EOF
+usage:  options
+
+OPTIONS:
+    -h Show this message
+    -r refseq ID
+    -t TADB path (defaults to $TAC_PATH/TADB)
+    -b BLAST path (optional if already on path)
+    -c TAClassifier path
+EOF
+}
+
+REFID=
+BLAST_PATH=
+TAC_PATH=
+
+while getopts "hr:b:c:" OPTION
+do
+    case $OPTION in
+    h)
+        usage
+        exit 1
+        ;;
+    r)
+        REFID=$OPTARG
+        ;;
+    b)
+        BLAST_PATH=$OPTARG
+        ;;
+    c)
+        TAC_PATH=$OPTARG
+        ;;
+    ?)
+        usage
+        exit
+        ;;
+    esac
+done
+
+if [[ -z $REFID ]] || [[ -z $TAC_PATH ]]
+then 
+    usage
+    exit 1
+fi
+
+set -o nounset
+set -o errexit
+
+## Resolve paths
+
+if [[ $BLAST_PATH ]]
+then
+    echo "Appending $BLAST_PATH to path"
+    export PATH=$PATH:$BLAST_PATH
+fi
+
+echo "Downloading gbk file..."
+GET_GBK="get_gbk.py"
+GBK=$REFID.gbk
+if [[ -f $GBK ]]
+then
+    echo "...$GBK exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$GET_GBK -r $REFID -o $GBK
+    set +o xtrace
+    echo "...done! Wrote $GBK"
+fi
+
+echo "Downloading essentiality file..."
+if [[ -f $REFID.essentiality.txt ]]
+then
+    echo "$REFID.essentiality.txt exists"
+else
+    set -o xtrace
+    wget http://cefg.uestc.edu.cn/geptop/prediction/$REFID.dat -O $REFID.essentiality.txt
+    set +o xtrace
+    echo "...done! Wrote $REFID.essentiality.txt"
+fi
+
+echo "Generating data mapping..."
+MAP="gen_mapping.py"
+if [[ -f $REFID.mapping.txt ]]
+then
+    echo "...$REFID.mapping.txt exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$MAP -g $GBK \
+        > $REFID.mapping.txt
+    set +o xtrace
+    echo "...done! Wrote $REFID.mapping.txt."
+fi
+
+
+echo "Getting .faa files..."
+GET_FAA="get_faa_from_map.py"
+GET_START="getStartingPositions.py"
+if [[ -f $REFID.toxin.faa ]] && [[ -f $REFID.antitoxin.faa ]]
+then
+    echo "...$REFID.*.faa exist"
+else
+    set -o xtrace
+    python $TAC_PATH/$GET_FAA -g $GBK -m $REFID.mapping.txt -o $REFID
+    set +o xtrace
+    echo "...done! Wrote $REFID.*.faa."
+    fi
+
+if [[ -f $REFID.startPositions.pkl ]]
+then
+    echo "...$REFID.startPositions.pkl exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$GET_START -o $REFID
+    set -o xtrace
+fi
+
+
+echo "Scoring..."
+ESSENTIALITY="score_essentiality.py"
+UPALINDROMES="score_upstream_palindromes.py"
+
+if [[ -f $REFID.features.essentiality.txt ]]
+then
+    echo "...$REFID.features.essentiality.txt exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$ESSENTIALITY -e $REFID.essentiality.txt -m $REFID.mapping.txt \
+        > $REFID.features.essentiality.txt
+    set +o xtrace
+    echo "...Wrote $REFID.essentiality.txt"
+    fi
+
+if [[ -f $REFID.features.upalindromes.txt ]]
+then
+    echo "...$REFID.features.upalindromes.txt exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$UPALINDROMES -g $GBK -m $REFID.mapping.txt \
+        > $REFID.features.upalindromes.txt
+    set +o xtrace
+    echo "...Wrote $REFID.features.upalindromes.txt"
+fi
+
+
+STRUCTURE="structure.py"
+HOMOLOGY="homology.py"
+PROPERTIES="properties.py"
+DATABASE=$TAC_PATH/phageDB/phage.genomes.fasta
+echo "Finding protein properties, gene structure, and phage homology ... "
+
+if [[ -f $REFID.features.structure.txt ]]
+then
+    echo "...$REFID.features.structure.txt exists"
+else
+    set -o xtrace
+    python $STRUCTURE -t $REFID.toxin.faa -a $REFID.antitoxin.faa \
+        -o $REFID.features
+    set +o xtrace
+    echo "...Wrote $REFID.features.structure.txt"
+fi
+
+if [[ -f $REFID.features.properties.txt ]]
+then
+    echo "...$REFID.features.properties.txt exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$PROPERTIES -t $REFID.toxin.faa -a $REFID.antitoxin.faa \
+            -o $REFID.features
+    set +o xtrace
+    echo "...Wrote $REFID.features.properties.txt"
+fi
+
+if [[ -f $REFID.features.homology.txt ]]
+then
+    echo "...$REFID.features.homology.txt exists"
+else
+    set -o xtrace
+    python $TAC_PATH/$HOMOLOGY -t $REFID.toxin.faa -a $REFID.antitoxin.faa \
+        -d $DATABASE -o $REFID
+    echo "#LOCUS HOMOLOGY:TOXIN HOMOLOGY:ANTITOXIN" > $REFID.features.homology.txt
+    gjoin -j 1 \
+        <(cat $REFID.homology.toxin.txt | tr "|" "\t" | cut -f 1,3 | sort) \
+        <(cat $REFID.homology.antitoxin.txt | tr "|" "\t" | cut -f 1,3 | sort) \
+        >> $REFID.features.homology.txt
+    set +o xtrace
+    echo "...Wrote $REFID.features.homology.txt"
+fi
+
---
+++ gen_mapping.py
@@ -0,0 +1,62 @@
+# Generate a negative mapping from the genbank file by filtering out
+# hypothetical proteins and proteins known to be in TA systems
+# Additional filters can be added. 
+
+import argparse
+
+from itertools import islice
+
+from Bio import SeqIO
+
+def window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    "http://stackoverflow.com/questions/6822725/rolling-or-sliding-window-iterator-in-python"
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result    
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+def gb_filter_it(features, filter_loci=[], filter_gi=[]):
+    for feature in features:
+        if feature.type == 'CDS':
+            if 'hypothetical' in feature.qualifiers['product'][0]:
+                continue
+            if ('locus_tag' in feature.qualifiers and 
+                set(feature.qualifiers['locus_tag']) & filter_loci):
+                continue
+            if ('gene' in feature.qualifiers and
+                set(feature.qualifiers['gene']) & filter_loci):
+                continue
+            if ('db_xref' in feature.qualifiers and 
+                set(feature.qualifiers['db_xref']) & filter_gi):
+                continue
+            yield feature
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--genbank')
+    args = parser.parse_args()
+
+    pos_loci, pos_gis = set(),set()
+
+    gb_handle = open(args.genbank, 'r')
+    gb_record = SeqIO.read(gb_handle, 'gb')
+    gb_record_id = gb_record.id
+    gb_handle.close()
+
+    get_gi = lambda feature:feature.qualifiers['db_xref'][0]
+    get_strand = lambda feature:feature.location.strand
+
+    print "#LOCUS GI:TOXIN GI:ANTITOXIN"
+    for i, (left, right) in enumerate(window(
+        gb_filter_it(gb_record.features, pos_loci, pos_gis))):
+        if abs(get_strand(left) + get_strand(right)) == 2:
+            print "{0}_N{1} {2} {3}".format(gb_record.id, i, get_gi(left), get_gi(right)) 
+
+
+if __name__ == '__main__':
+    main()
